943552e7037e0094a5b5c6937cacf8cf44aadda3
Kumar Shraiyash <justshraiyash@gmail.com>
2021-03-07 04:35:30 +0530

    first commit

---
+++ build/precache-manifest.4a6767efbd3c89b230ff0b0460247e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f76f9e78a27ecd02607afed4823a36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9516afa4053cdba45508",
+    "url": "/static/css/main.04a85805.chunk.css"
+  },
+  {
+    "revision": "08d2aa5062f4ff618d38",
+    "url": "/static/js/2.4801396f.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.4801396f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9516afa4053cdba45508",
+    "url": "/static/js/main.bf04625c.chunk.js"
+  },
+  {
+    "revision": "2b615c63248548e061fc",
+    "url": "/static/js/runtime-main.09d4436f.js"
+  }
+]);
